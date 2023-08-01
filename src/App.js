ef51d6ae8d5de612023324f8d186fd403b43b2b4
pengqiang.li <pengqiang.li@accenture.com>
2023-08-01 19:43:38 +0800

    antd app configProvider locale

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import { App as AntApp, ConfigProvider } from 'antd';
+import zhCN from 'antd/locale/zh_CN';
+import 'dayjs/locale/zh-cn';
+import { useState } from 'react';
 
 function App() {
+
+  const [locale] = useState(zhCN);
+
   return (
     <div className="App">
+
+      <ConfigProvider
+        locale={locale}
+        componentSize='small'
+        theme={
+          {
+            token: {
+              colorTextHeading: 'grey',
+              colorPrimary: '#006564'
+            }
+          }
+        }
+      >
+        <AntApp>
+
+        </AntApp>
+      </ConfigProvider>
       
     </div>
   );
