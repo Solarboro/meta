af9794a7575200af4c49ba7d8ca3bb67e019f6f0
pengqiang.li <pengqiang.li@accenture.com>
2023-08-20 22:43:53 +0800

    account test on IMEI

--- src/pages/account/index.js
+++ src/pages/account/index.js
@@ -1,12 +1,27 @@
+import { Divider } from "antd";
+import { useEffect } from "react"
 
 
 
 const AccountPage = () =>{
 
 
+    useEffect(
+        ()=>{
+
+            console.log(window)
+            console.log(navigator)
+        },[]
+    );
+
     return (
         <div>
-            Account here
+            
+            {JSON.stringify(navigator.userAgent)}
+            <Divider/>
+            {window?.AndroidWebView?.getIMEI()||"unkown by android"}
+            <Divider/>
+            {window?.webkit?.messageHandlers?.getIMEI?.postMessage(null)||"unkown by IOS"}
         </div>
     )
 }
